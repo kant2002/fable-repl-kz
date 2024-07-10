d74e7687006aa00702895193b5c41d9029c5c5b2
Andrii Kurdiumov <kant2002@gmail.com>
2024-07-10 15:03:12 +0500

    A bit of translation

--- samples/tour/functions.fs
+++ samples/tour/functions.fs
@@ -4,39 +4,39 @@ module Tour.Functions
 // Visit the link above for more information on each topic
 // You can also find more learning resources at https://fsharp.org/
 
-module BasicFunctions =
+module НегізгіФункциялар =
 
     /// You use 'let' to define a function. This one accepts an integer argument and returns an integer.
     /// Parentheses are optional for function arguments, except for when you use an explicit type annotation.
-    let sampleFunction1 x = x*x + 3
+    let үлгіФункция1 x = x*x + 3
 
     /// Apply the function, naming the function return result using 'let'.
     /// The variable type is inferred from the function return type.
-    let result1 = sampleFunction1 4573
+    let нәтиже1 = үлгіФункция1 4573
 
     // This line uses '%d' to print the result as an integer. This is type-safe.
-    // If 'result1' were not of type 'int', then the line would fail to compile.
-    printfn "The result of squaring the integer 4573 and adding 3 is %d" result1
+    // If 'нәтиже1' were not of type 'int', then the line would fail to compile.
+    printfn "4573 бүтін санын квадраттап, 3-ті қосқанда %d шығады" нәтиже1
 
     /// When needed, annotate the type of a parameter name using '(argument:type)'.  Parentheses are required.
-    let sampleFunction2 (x:int) = 2*x*x - x/5 + 3
+    let үлгіФункция2 (x:int) = 2*x*x - x/5 + 3
 
-    let result2 = sampleFunction2 (7 + 4)
-    printfn "The result of applying the 2nd sample function to (7 + 4) is %d" result2
+    let нәтиже2 = үлгіФункция2 (7 + 4)
+    printfn "(7 + 4) 2 таңдау функциясын қолдану нәтижесі %d болады" нәтиже2
 
     /// Conditionals use if/then/elif/else.
     ///
     /// Note that F# uses white space indentation-aware syntax, similar to languages like Python.
-    let sampleFunction3 x =
+    let үлгіФункци3 x =
         if x < 100.0 then
             2.0*x*x - x/5.0 + 3.0
         else
             2.0*x*x + x/5.0 - 37.0
 
-    let result3 = sampleFunction3 (6.5 + 4.5)
+    let нәтиже3 = үлгіФункци3 (6.5 + 4.5)
 
     // This line uses '%f' to print the result as a float.  As with '%d' above, this is type-safe.
-    printfn "The result of applying the 3rd sample function to (6.5 + 4.5) is %f" result3
+    printfn "The result of applying the 3rd sample function to (6.5 + 4.5) is %f" нәтиже3
 
 
 module Immutability =
